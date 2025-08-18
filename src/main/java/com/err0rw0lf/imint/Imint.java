be1b10dc3047ec24f7ebdbc40501bf1c6e336d2a
ERR0RW0LF <finn.woma@gmail.com>
2025-08-18 19:32:13 +0200

    initialize Fabric mod structure with essential configuration files

---
+++ src/main/java/com/err0rw0lf/imint/Imint.java
@@ -0,0 +1,10 @@
+package com.err0rw0lf.imint;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Imint implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
