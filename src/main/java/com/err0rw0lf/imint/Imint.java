46ba08524f5ae8e6f55cea840b278a35d882c19a
ERR0RW0LF <finn.woma@gmail.com>
2025-08-18 23:18:54 +0200

    Revert "initialize Fabric mod structure with essential configuration files"
    This reverts commit e26a1b9d4cd9621a84235c57ab179d501bc40097.

--- src/main/java/com/err0rw0lf/imint/Imint.java
+++ src/main/java/com/err0rw0lf/imint/Imint.java
@@ -1,13 +1,8 @@
 package com.err0rw0lf.imint;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Imint implements ModInitializer {
-
-    public static final String MOD_ID = "imint";
-    public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
