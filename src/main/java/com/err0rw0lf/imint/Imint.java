5b5f5f3c1c1e60c94769fc105adeb107aea4426a
ERR0RW0LF <finn.woma@gmail.com>
2025-08-19 15:44:49 +0200

    Add Mint Leave item and localization support

--- src/main/java/com/err0rw0lf/imint/Imint.java
+++ src/main/java/com/err0rw0lf/imint/Imint.java
@@ -1,10 +1,17 @@
 package com.err0rw0lf.imint;
 
+import com.err0rw0lf.imint.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Imint implements ModInitializer {
 
+    public static final String MOD_ID = "imint";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
     }
 }
--- src/main/java/com/err0rw0lf/imint/item/ModItems.java
+++ src/main/java/com/err0rw0lf/imint/item/ModItems.java
@@ -1,4 +1,37 @@
 package com.err0rw0lf.imint.item;
 
+import com.err0rw0lf.imint.Imint;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
 public class ModItems {
+    public static final Item MINT_LEAVE = register("mint_leave", Item::new, new Item.Settings());
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Imint.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+    public static void registerModItems() {
+        Imint.LOGGER.info("Registering Mod Items for " + Imint.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(MINT_LEAVE);
+        });
+    }
 }
